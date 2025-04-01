e568b5f417615709b3af91d514d1b4a5729862b5
Andrew Becker <andrew.becker.93@gmail.com>
2025-04-01 16:46:38 -0400

    come on

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 // src/app/layout.tsx
-import "../globals.css";
-import Layout from "../components/layout";
+import "globals.css";
+import Layout from "components/layout";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
